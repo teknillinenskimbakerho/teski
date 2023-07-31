13fe60202c2dbc4ab92e0c275974821336b8b3a0
teknillinenskimbakerho <140888269+teknillinenskimbakerho@users.noreply.github.com>
2023-07-31 11:17:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const App = () => (
 );
 
 const BackgroundContainer = styled.div`
-  background-image: url('https://github.com/teknillinenskimbakerho/teski/blob/gh-pages/public/teski.png.png?raw=true');
+  background-image: url('https://i.imgur.com/53BiitD.png');
   background-size: 30%; /* Change 'cover' to 'contain', '100%', or specific dimensions as needed */
   background-repeat: no-repeat;
   background-position: center bottom 20%;
