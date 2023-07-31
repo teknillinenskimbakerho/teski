95f5c2dcca63c54ec2f594e5d022953aec2df368
teknillinenskimbakerho <140888269+teknillinenskimbakerho@users.noreply.github.com>
2023-07-31 10:30:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const App = () => (
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BackgroundContainer>
-    <BackgroundContainer>
+    </BackgroundContainer>
   </Router>
 );
 
